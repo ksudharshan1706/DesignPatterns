bd03f983f57b77fd72845bbc42a68f9a5a3cc70a
sudharshan kamavaram <sudarshan.kamavaram@opezee.com>
2024-09-18 17:30:00 +0530

    Code commit for Adapter DP implementation.

---
+++ DesignPatterns/AdapterDP/AdapterDp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AdapterDP
+{
+    public class RoundHole
+    {
+        public double radius;
+        public RoundHole(double rradius)
+        {
+            this.radius = rradius;
+        }
+        public double getRadius()
+        {
+            return this.radius;
+
+        }
+        public bool fits(RoundPeg peg)
+        {
+            return this.radius >= peg.getRadius();
+        }
+    }
+
+    public class RoundPeg
+    {
+        public double radius;
+        public RoundPeg(double radius)
+        {
+            this.radius = radius;
+        }
+        public virtual double getRadius()
+        {
+            return this.radius;
+        }
+    }
+
+    public class SquarePeg
+    {
+        public double width;
+        public SquarePeg(double width)
+        {
+            this.width = width;
+        }
+        public double getWidth()
+        {
+            return this.width;
+        }
+
+    }
+
+    public class AdapterDp : RoundPeg
+    {
+        public SquarePeg SquarePeg;
+        public AdapterDp(SquarePeg peg) : base(0)
+        {
+            this.SquarePeg = peg;
+        }
+
+        public override double getRadius()
+        {
+            return this.SquarePeg.getWidth() * Math.Sqrt(2) / 2;
+        }
+    }
+
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPatterns.AdapterDP;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
+
+
+            //Adapter DP
+            
+            RoundHole roundHole = new RoundHole(5);
+            Console.WriteLine("Hole Radius : "+roundHole.getRadius().ToString());
+
+            RoundPeg roundPeg = new RoundPeg(5);
+            Console.WriteLine("peg Radius : " + roundPeg.getRadius().ToString());
+
+            RoundPeg roundPeg_large = new RoundPeg(10);
+            Console.WriteLine("peg Radius : " + roundPeg_large.getRadius().ToString());
+
+            Console.WriteLine("will the peg fit in Round hole : " + roundHole.fits(roundPeg));
+            Console.WriteLine("will the Large peg fit in Round hole : " + roundHole.fits(roundPeg_large));
+
+            SquarePeg squarePeg = new SquarePeg(5);
+            //roundHole.fits(squarePeg);
+            //incompatible because the roundHole only takes only RoundPeg object
+
+            // trying to achieve it with adapter.
+            AdapterDp adapterDp = new AdapterDp(squarePeg);
+            Console.WriteLine("get adapter radius : " + adapterDp.getRadius());
+            Console.WriteLine("will the adapter fit in Round hole : " + roundHole.fits(adapterDp));
+
+
+            Console.ReadLine();
         }
     }
 }
