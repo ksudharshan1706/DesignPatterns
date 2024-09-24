4f21e82cb53761f98080a45689f85d935ebc6891
sudharshan kamavaram <sudarshan.kamavaram@opezee.com>
2024-09-24 19:17:59 +0530

    Code Commit for Bridge DP

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DesignPatterns.AdapterDP;
+using DesignPatterns.StructuralDP.Bridge_DP;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,29 +13,34 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
 
+            //Bridge DP
+            BridgeDP bridgeDP = new BridgeDP();
+            bridgeDP.Initialize();
+
+
 
             //Adapter DP
             
-            RoundHole roundHole = new RoundHole(5);
-            Console.WriteLine("Hole Radius : "+roundHole.getRadius().ToString());
+            //RoundHole roundHole = new RoundHole(5);
+            //Console.WriteLine("Hole Radius : "+roundHole.getRadius().ToString());
 
-            RoundPeg roundPeg = new RoundPeg(5);
-            Console.WriteLine("peg Radius : " + roundPeg.getRadius().ToString());
+            //RoundPeg roundPeg = new RoundPeg(5);
+            //Console.WriteLine("peg Radius : " + roundPeg.getRadius().ToString());
 
-            RoundPeg roundPeg_large = new RoundPeg(10);
-            Console.WriteLine("peg Radius : " + roundPeg_large.getRadius().ToString());
+            //RoundPeg roundPeg_large = new RoundPeg(10);
+            //Console.WriteLine("peg Radius : " + roundPeg_large.getRadius().ToString());
 
-            Console.WriteLine("will the peg fit in Round hole : " + roundHole.fits(roundPeg));
-            Console.WriteLine("will the Large peg fit in Round hole : " + roundHole.fits(roundPeg_large));
+            //Console.WriteLine("will the peg fit in Round hole : " + roundHole.fits(roundPeg));
+            //Console.WriteLine("will the Large peg fit in Round hole : " + roundHole.fits(roundPeg_large));
 
-            SquarePeg squarePeg = new SquarePeg(5);
-            //roundHole.fits(squarePeg);
-            //incompatible because the roundHole only takes only RoundPeg object
+            //SquarePeg squarePeg = new SquarePeg(5);
+            ////roundHole.fits(squarePeg);
+            ////incompatible because the roundHole only takes only RoundPeg object
 
-            // trying to achieve it with adapter.
-            AdapterDp adapterDp = new AdapterDp(squarePeg);
-            Console.WriteLine("get adapter radius : " + adapterDp.getRadius());
-            Console.WriteLine("will the adapter fit in Round hole : " + roundHole.fits(adapterDp));
+            //// trying to achieve it with adapter.
+            //AdapterDp adapterDp = new AdapterDp(squarePeg);
+            //Console.WriteLine("get adapter radius : " + adapterDp.getRadius());
+            //Console.WriteLine("will the adapter fit in Round hole : " + roundHole.fits(adapterDp));
 
 
             Console.ReadLine();
---
+++ DesignPatterns/StructuralDP/Bridge DP/BridgeDP.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralDP.Bridge_DP
+{
+    public interface IDevice
+    {
+        void enable();
+        void disable();
+        void getVolume();
+        void setVolume(double volume);
+
+    }
+    public class RemoteControl
+    {
+        private IDevice device;
+        public RemoteControl(IDevice device)
+        {
+            this.device = device;
+        }
+
+        public void enable()
+        {
+            this.device.enable();
+        }
+        public void disable()
+        {
+            this.device.disable();
+        }
+        public void getVolume()
+        {
+            this.device.getVolume();
+        }
+        public void setVolume(double volume)
+        {
+            this.device.setVolume(volume);
+        }
+
+    }
+
+    public class AdvancedRemoteControl : RemoteControl
+    {
+        public IDevice device;
+        public AdvancedRemoteControl(IDevice device) : base(device)
+        {
+            this.device = device;   
+        }
+
+        public void mute()
+        {
+            this.device.setVolume(0);
+        }
+    }
+
+    public class TV : IDevice
+    {
+        public void enable()
+        {
+            Console.WriteLine("TV enabled");
+        }
+        public void disable()
+        {
+            Console.WriteLine("TV Disabled");
+        }
+
+        public void getVolume()
+        {
+            Console.WriteLine("Get TV Volume");
+        }
+
+        public void setVolume(double volume)
+        {
+            Console.WriteLine($"TV Volume set to {volume}");
+        }
+    }
+
+
+    public class Radio : IDevice
+    {
+        public void enable()
+        {
+            Console.WriteLine("Radio enabled");
+        }
+        public void disable()
+        {
+            Console.WriteLine("Radio Disabled");
+        }
+
+        public void getVolume()
+        {
+            Console.WriteLine("Get Radio Volume");
+        }
+
+        public void setVolume(double volume)
+        {
+            Console.WriteLine($"Radio Volume set to {volume}");
+        }
+    }
+
+
+    public class BridgeDP
+    {
+        public TV tv;
+        public Radio Radio;
+        public RemoteControl RemoteControl;
+        public AdvancedRemoteControl AdvancedRemoteControl;
+        public BridgeDP()
+        {
+            tv = new TV();  // Instantiate the TV object
+            RemoteControl = new RemoteControl(tv);  // Pass the TV object to the RemoteControl
+
+            Radio = new Radio();
+            AdvancedRemoteControl = new AdvancedRemoteControl(Radio);
+
+        }
+
+        public void Initialize()
+        {
+            RemoteControl.enable();  // Call the enable method on the RemoteControl
+            AdvancedRemoteControl.mute();
+            AdvancedRemoteControl.enable();
+        }
+    }
+}
