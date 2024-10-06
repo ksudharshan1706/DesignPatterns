c164d814b1d1a2153606613442213e573dc3ea14
sudharshan kamavaram <sudarshan.kamavaram@opezee.com>
2024-10-06 18:09:21 +0530

    Code commit for Real time example of Coffee Flyweight DP

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,9 +1,9 @@
 ﻿using DesignPatterns.AdapterDP;
 using DesignPatterns.StructuralDP.AdapterDP;
-using DesignPatterns.StructuralDP.Decorator_DP;
 using DesignPatterns.StructuralDP.Facade_DP;
 using DesignPatterns.StructuralDP.Facade_DP.RealTimeExample;
 using DesignPatterns.StructuralDP.Flyweight_DP;
+using DesignPatterns.StructuralDP.Flyweight_DP.RealTimeExample;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,47 +23,15 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
 
-            //Flyweight DP
+            //Coffee Flyweight DP
 
-            Console.WriteLine("\n Red color Circles ");
-            for (int i = 0; i < 3; i++)
-            {
-                Circle circle = (Circle)ShapeFactory.GetShape("circle");
-                circle.setColor("Red");
-                circle.Draw();
-            }
-            //Creating Circle Objects with Green Color
-            Console.WriteLine("\n Green color Circles ");
-            for (int i = 0; i < 3; i++)
-            {
-                Circle circle = (Circle)ShapeFactory.GetShape("circle");
-                circle.setColor("Green");
-                circle.Draw();
-            }
-            //Creating Circle Objects with Blue Color
-            Console.WriteLine("\n Blue color Circles");
-            for (int i = 0; i < 3; ++i)
-            {
-                Circle circle = (Circle)ShapeFactory.GetShape("circle");
-                circle.setColor("Green");
-                circle.Draw();
-            }
-            //Creating Circle Objects with Orange Color
-            Console.WriteLine("\n Orange color Circles");
-            for (int i = 0; i < 3; ++i)
-            {
-                Circle circle = (Circle)ShapeFactory.GetShape("circle");
-                circle.setColor("Orange");
-                circle.Draw();
-            }
-            //Creating Circle Objects with Black Color
-            Console.WriteLine("\n Black color Circles");
-            for (int i = 0; i < 3; ++i)
-            {
-                Circle circle = (Circle)ShapeFactory.GetShape("circle");
-                circle.setColor("Black");
-                circle.Draw();
-            }
+            var shop = new CoffeeShop();
+            shop.TakeOrders("Cappuccino", 1);
+            shop.TakeOrders("Espresso", 2, "With extra sugar");
+            shop.TakeOrders("Cappuccino", 3);
+            shop.TakeOrders("Latte", 4);
+            shop.ServeOrder();
+
             Console.ReadKey();
 
         }
---
+++ DesignPatterns/StructuralDP/Flyweight DP/RealTimeExample/CoffeeFlyweightDp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralDP.Flyweight_DP.RealTimeExample
+{
+    public class Coffee
+    {
+        public string Flavour { get; set; }
+        public string Ingredients { get; set; }
+        public string Preparation {  get; set; }
+
+        public Coffee(string flavour, string ingredients, string preparation) 
+        {
+            Flavour = flavour;
+            Ingredients = ingredients;
+            Preparation = preparation;
+        }
+
+        public void serveCoffee(int tableNumber,string Customisations = "")
+        {
+            Console.WriteLine($"Serving {Flavour} coffee (made with {Ingredients} and {Preparation}) to table {tableNumber}. {Customisations}");
+        }
+    }
+
+    public class CoffeeFactory
+    {
+        private readonly Dictionary<string, Coffee> _coffee = new Dictionary<string, Coffee>();
+
+        public Coffee GetCoffee(string flavour) 
+        {
+            if(!_coffee.ContainsKey(flavour))
+            {
+                var coffee = new Coffee(flavour, "water", "brewed");
+                _coffee.Add(flavour, coffee);
+            }
+            return _coffee[flavour];
+        }
+    }
+
+    public class CoffeeShop
+    {
+        private readonly CoffeeFactory coffeeFactory = new CoffeeFactory();
+        private readonly List<Tuple<Coffee,int,string>> _orders = new List<Tuple<Coffee, int, string>>();
+
+        public void TakeOrders(string flavour,int tablenumber,string customisations = "")
+        {
+            var coffee = coffeeFactory.GetCoffee(flavour);
+            _orders.Add(Tuple.Create(coffee, tablenumber, customisations));
+        }
+
+        public void ServeOrder()
+        {
+            foreach( var order in _orders)
+            {
+                order.Item1.serveCoffee(order.Item2, order.Item3);
+            }
+            _orders.Clear();
+        }
+
+    }
+}
